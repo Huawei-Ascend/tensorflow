6d34f6f64194eb8b39f2c542250480b5e2dcb4b3
Yanqingshang <y00562645@china.huawei.com>
2020-10-30 20:02:27 +0800

    syn yellow to blue

--- cmake/tensorflow.cmake
+++ cmake/tensorflow.cmake
@@ -1,10 +1,19 @@
 include(FetchContent)
-
-FetchContent_Declare(
-        tensorflow
-        URL https://github.com/tensorflow/tensorflow/archive/v1.15.0.zip
-        URL_HASH MD5=0ad811d8d59f56ecc1a6032af997ec1d
-)
+set(_json_url "")
+if(TF_PKG_SERVER)
+  set(_json_url "${TF_PKG_SERVER}/libs/tensorflow/v1.15.0.zip")
+  FetchContent_Declare(
+          tensorflow
+          URL ${_json_url}
+          URL_HASH MD5=0ad811d8d59f56ecc1a6032af997ec1d
+  )
+else()
+  FetchContent_Declare(
+          tensorflow
+          URL https://github.com/tensorflow/tensorflow/archive/v1.15.0.zip
+          URL_HASH MD5=0ad811d8d59f56ecc1a6032af997ec1d
+  )
+endif()
 FetchContent_GetProperties(tensorflow)
 if (NOT tensorflow_POPULATED)
     FetchContent_Populate(tensorflow)
