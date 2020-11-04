5defad8e82f11e93113a5cbef231eed3186a6ec6
cclworkaccount <8266062+cclworkaccount@user.noreply.gitee.com>
2020-11-04 15:17:58 +0800

    update tf_adapter/python/npu_bridge/hccl/hccl_ops.py. HCCL broadcast融合，tfadptor python 接口更改

--- tf_adapter/python/npu_bridge/hccl/hccl_ops.py
+++ tf_adapter/python/npu_bridge/hccl/hccl_ops.py
@@ -48,11 +48,15 @@ def allgather(tensor, rank_size, group="hccl_world_group"):
 ## 提供group内的集合通信broadcast功能
 #  @param tensor tensorflow的tensor类型，broadcast操作的输入；
 #  @param root_rank int类型，作为root节点的rank_id，该id是group内的rank id;
+#  @param fusion int类型，算子融合标识。0: 不融合；1:按照相同fusion_id融合;其他值非法。
+#  @param fusion_id int类型，算子融合索引标识，相同fusion_id的算子将会融合。
 #  @param group string类型，group名称，可以为用户自定义group或者"hccl_world_group";
 #  @return 对输入tensor执行完broadcast操作之后的结果tensor
-def broadcast(tensor, root_rank, group="hccl_world_group"):
+def broadcast(tensor, root_rank, fusion=0, fusion_id=-1, group="hccl_world_group"):
     result = gen_hccl_ops.hcom_broadcast(
         input=tensor,
+        fusion=fusion,
+        fusion_id=fusion_id,
         group=group,
         root_rank=root_rank)
     return result
